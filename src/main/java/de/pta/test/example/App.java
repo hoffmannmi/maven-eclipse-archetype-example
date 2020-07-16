0ce9d9266ca35f862b04069b98807f69baa97bf6
hoffmannm <michael.hoffmann@pta.de>
2020-07-16 09:52:48 +0200

    added dependencies to maven-quick-start added plugin to create an executable jar

--- src/main/java/de/pta/test/example/App.java
+++ src/main/java/de/pta/test/example/App.java
@@ -1,5 +1,7 @@
 package de.pta.test.example;
 
+import clinic.programming.training.Application;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Application app = new Application();
+        app.countWords("Hello World");
     }
 }
